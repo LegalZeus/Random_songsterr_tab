4ebee3f04f174b3c705c82dd53889972c45d7881
LegalZeus <rmichele288@gmail.com>
2020-07-12 20:03:45 +0200

    .

---
+++ index.js
@@ -0,0 +1,20 @@
+function main() {
+    artist_name = document.getElementById("inp").value;
+
+    fetch("http://www.songsterr.com/a/ra/songs.json?pattern=" + artist_name)
+        .then(res => res.json())
+        .then(res => {
+            let item = res[Math.floor(Math.random() * res.length)];
+            console.log(item)
+            artist = item['artist'].name
+            song_title = item.title
+
+            console.log(`${song_title} - (${artist})`)
+            document.getElementById("out").innerText = `${song_title} - (${artist})`
+
+            let song_link = `http://www.songsterr.com/a/wa/bestMatchForQueryString?s={${song_title}}&a={${artist}}`
+            document.getElementById("tab-link").setAttribute("href", song_link)
+            console.log(song_link)
+
+        })
+}
