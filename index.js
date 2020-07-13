79f4b2f7adb0b003f06b32557842b981b624cb79
LegalZeus <rmichele288@gmail.com>
2020-07-13 20:25:55 +0200

    .

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ function main() {
     artist_name = document.getElementById("inp").value;
 
     fetch("//www.songsterr.com/a/ra/songs.json?pattern=" + artist_name)
-        .then(res => res.json())
+
+    fetch("https://www.songsterr.com/a/ra/songs.json?pattern=" + artist_name)
+
+    fetch("//www.songsterr.com/a/ra/songs.json?pattern=" + artist_name)
+
+    .then(res => res.json())
         .then(res => {
             let item = res[Math.floor(Math.random() * res.length)];
             console.log(item)
@@ -12,8 +17,11 @@ function main() {
             console.log(`${song_title} - (${artist})`)
             document.getElementById("out").innerText = `${song_title} - (${artist})`
 
+
             let song_link = `//www.songsterr.com/a/wa/bestMatchForQueryString?s={${song_title}}&a={${artist}}`
+
             document.getElementById("tab-link").setAttribute("href", song_link)
+            document.getElementById("tab-link").style.visibility = "visible"
             console.log(song_link)
 
         })
